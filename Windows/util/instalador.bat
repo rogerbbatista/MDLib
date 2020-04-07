828ceacc1db4b3420a0dd649e87d453c03e8fb42
ArthurCarmo <37702237+ArthurCarmo@users.noreply.github.com>
2020-04-07 16:15:20 -0300

    Update instalador.bat

--- Windows/util/instalador.bat
+++ Windows/util/instalador.bat
@@ -1,10 +1,5 @@
 TITLE MDLib Windows
-ECHO Habilitando a máquina virtual Linux
+ECHO Habilitando a maquina virtual Linux
 
-:: Para permitir a instalação da WSL
+:: Para permitir a instalacao da WSL
 PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command ""Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux""' -Verb RunAs}"
-
-ECHO Instalando as dependências do sistema...
-
-:: Para executar o instalador de dependências automaticamente
-python3 %~dp0pip_install_win.py
